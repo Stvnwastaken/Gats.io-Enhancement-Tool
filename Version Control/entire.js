8f5f764de959bdba528168d0110e00ff6b29274f
LightLord <87213208+LightLordYT@users.noreply.github.com>
2021-08-21 09:14:44 -0700

    Update entire.js

--- Version Control/entire.js
+++ Version Control/entire.js
@@ -7,8 +7,10 @@
 // @icon         https://www.google.com/s2/favicons?domain=gats.io
 // @description  Gats.io-Enhancement-Tool (G.E.T) This is a script for Gats.io that customizes the gui This is still in the Beta stage so don't expect it to be a completely developed, and perfect script.
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
-// @updateURL    https://greasyfork.org/en/scripts/430140-g-e-t-gats-enhancement-tool-beta-version-1-4-8
-// @downloadURL  https://greasyfork.org/en/scripts/430140-g-e-t-gats-enhancement-tool-beta-version-1-4-8
+// @require      https://cdn.jsdelivr.net/gh/LightLordYT/Gats.io-Enhancement-Tool@latest/code.min.js
+// @connect      https://cdn.jsdelivr.net/gh/LightLordYT/Gats.io-Enhancement-Tool@latest/code.min.js
+// @updateURL    https://github.com/LightLordYT/Gats.io-Enhancement-Tool/blob/main/get.js
+// @downloadURL  https://github.com/LightLordYT/Gats.io-Enhancement-Tool/blob/main/get.js
 // @grant        GM_addStyle
 // @compatible   chrome+tampermonkey
 // ==/UserScript==
