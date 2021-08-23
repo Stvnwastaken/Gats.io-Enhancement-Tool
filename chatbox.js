a1a4caaa16635f4e0669d572799755c1c34fbb9a
LightLord <87213208+LightLordYT@users.noreply.github.com>
2021-08-23 16:06:01 -0700

    Delete chatbox.js

--- chatbox.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("chatbox").maxlength("100");
-document.getElementById("chatbox").style.borderRadius = "25px";
-document.getElementById("chatbox").style.backgroundColor = "blue";
-document.getElementById("chatbox").style.borderColor = "black";
-document.getElementById("chatbox").style.color = "white";
