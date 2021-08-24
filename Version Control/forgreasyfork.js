f9dd39623237d54cb176a18f599abc3cc1c8c694
LightLord <87213208+LightLordYT@users.noreply.github.com>
2021-08-23 17:00:18 -0700

    Create forgreasyfork.js

---
+++ Version Control/forgreasyfork.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         G.E.T Gats Enhancement Tool Beta Version 1.7.8 greasyfork
+// @namespace    https://github.com/LightLordYT/
+// @version      Beta 1.7.8
+// @author       LightLord
+// @match        https://gats.io/
+// @icon         https://www.google.com/s2/favicons?domain=gats.io
+// @description  Gats.io-Enhancement-Tool (G.E.T) This is a script for Gats.io that customizes the gui This is still in the Beta stage so don't expect it to be a completely developed, and perfect script.
+// @updateURL    https://github.com/LightLordYT/Gats.io-Enhancement-Tool/raw/main/get.js
+// @downloadURL  https://github.com/LightLordYT/Gats.io-Enhancement-Tool/raw/main/get.js
+// @grant        GM_addStyle
+// @compatible   chrome+tampermonkey
+// ==/UserScript==
+
+
+ /* Version Updates:
+    V1.0.0 Alpha version release
+    V1.0.1 Alpha version added title
+    V1.0.2 Added other customization stuff
+    V1.0.3 Disabled canvas to fix glitch
+    V1.0.4 Fixed css for top scores and play also credit for myself XD
+    V1.0.5 Checks for update
+    V1.1.5 Redid gui also did some changelog/alert/stats stuff
+    V1.1.6 Made the homescreen customization match
+    V1.2.6 Added Tips and Tricks and Combos for each gun
+    V1.3.6 Added fps,ms,bytes counter
+    V1.4.7 Added jQuery and added other stuff
+    V1.4.8 Added compatibility
+    V1.5.8 Added source control from github
+    V1.6.8 Optimized adblock and customization
+    V1.7.8 Added gui and optimized code*/
+
+
+
+
+stop();
+
+document.open('text/html', 'replace');
+document.write(`
+<html>
+<h1>This gats enhancement tool script is not supported, for the offical script go here:</h1>
+<div>
+<a target="_blank" href="https://github.com/LightLordYT/Gats.io-Enhancement-Tool/blob/main/get.js">https://github.com/LightLordYT/Gats.io-Enhancement-Tool/blob/main/get.js</a>
+</div>
+  <body>INSTALLATION: Go to the link above and copy all the code. Then click the tampermonkey extension and create a new script. delete all the code in the new script and paste the new code in (that was from the github link)  </body>
+</html>
+`);
